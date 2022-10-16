e7756fa6d25256ed2782dcad5d4ba59501ead01b
potapag <potapag@gmail.com>
2022-10-16 13:26:30 +0300

    navBar

--- src/app/App.js
+++ src/app/App.js
@@ -4,15 +4,17 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Users from './layouts/users';
 import Login from './layouts/login';
 import Main from './layouts/main';
+import NavBar from './components/navBar';
 
 function App() {
     return (
         <div>
+            <NavBar />
             <Switch>
                 <Route path="/users" component={Users} />
                 <Route path="/login" component={Login} />
                 <Route path="/" exact component={Main} />
-                <Redirect to='/' />
+                <Redirect to="/" />
             </Switch>
         </div>
     );
