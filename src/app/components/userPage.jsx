ab720b93c14617224f91233703d72b980ccfae50
potapag <potapag@gmail.com>
2022-10-17 15:24:22 +0300

    custom pages

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import api from '../api';
+import QualitiesList from './qualitiesList';
 
 const UserPage = ({ userId }) => {
-    return <h1>UserPage {userId}</h1>;
+    const history = useHistory();
+    const [user, setUsers] = useState();
+    useEffect(() => {
+        api.users.getById(userId).then((data) => setUsers(data));
+    });
+    const handleClick = () => {
+        history.push('/users');
+    };
+
+    if (user) {
+        return (
+            <div>
+                <h1>{user.name}</h1>
+                <h2>Профессия: {user.profession.name}</h2>
+                <QualitiesList qualities={user.qualities} />
+                <p>Завершенных встреч: {user.completedMeetings}</p>
+                <h2>Оценка: {user.rate} </h2>
+                <button onClick={handleClick}>Все пользователи</button>
+            </div>
+        );
+    } else {
+        return <h1>Loading</h1>;
+    }
 };
 
 UserPage.propTypes = { userId: PropTypes.string.isRequired };
