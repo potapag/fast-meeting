31d20c1edd52b79828fc476eb866818933c9a29e
potapag <potapag@gmail.com>
2022-10-20 14:39:29 +0300

    handling form values

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [data, setData] = useState({ email: '', password: '' });
+    const handelChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
     return (
         <form action="">
             <div className="">
                 <label htmlFor="email" className="">
                     Email
                 </label>
-                <input type="text" className="" id="email" />
+                <input
+                    type="text"
+                    className=""
+                    id="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handelChange}
+                />
             </div>
             <div>
                 <label htmlFor="password">Пароль</label>
-                <input type="password" id="password" />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handelChange}
+                />
             </div>
         </form>
     );
