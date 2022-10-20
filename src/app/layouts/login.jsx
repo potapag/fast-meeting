8715abea4f9dd23c1523df3ccec6bfc0317a5f5a
potapag <potapag@gmail.com>
2022-10-20 11:37:45 +0300

    form login

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Login = () => {
-    return <h1>Login</h1>;
+    return (
+        <form action="">
+            <div className="">
+                <label htmlFor="email" className="">
+                    Email
+                </label>
+                <input type="text" className="" id="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Пароль</label>
+                <input type="password" id="password" />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
