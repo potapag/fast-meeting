6922f2c4f78ded5beae5253d18d7c95d554f42c2
potapag <potapag@gmail.com>
2022-10-24 10:27:32 +0300

    basic validation

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '../components/textField';
 
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' });
+    const [, setErrors] = useState();
     const handelChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = {};
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === '') {
+                errors[fieldName] = `${fieldName} обязательно для заполнения`;
+            }
+        }
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
         console.log(data);
     };
 
