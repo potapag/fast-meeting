f9d24c8b0842d2ca5362962d02a0c3be59443268
potapag <potapag@gmail.com>
2022-10-26 13:13:13 +0300

    disabling submit button

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -42,6 +42,7 @@ const Login = () => {
         setErrors(errors);
         return Object.keys(errors).length === 0;
     };
+    const isValid = Object.keys(errors).length === 0;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -67,7 +68,9 @@ const Login = () => {
                 onChange={handelChange}
                 error={errors.password}
             />
-            <button>Submit</button>
+            <button type="submit" disabled={!isValid}>
+                Submit
+            </button>
         </form>
     );
 };
