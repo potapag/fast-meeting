8c9672d8663c5ec693dd6be69a5b483db81c2930
potapag <potapag@gmail.com>
2022-12-22 15:11:58 +0300

    user page design update debug 22.12

--- src/app/components/ui/userCard.jsx
+++ src/app/components/ui/userCard.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 const UserCard = ({ user }) => {
     const history = useHistory();
-    const hadleClick = () => {
+    const handleClick = () => {
         history.push(history.location.pathname + '/edit');
     };
     return (
         <div className="card mb-3">
-            <div className="caed-body">
+            <div className="card-body">
                 <button
                     className="position-absolute top-0 end-0 btn btn-light btn-sm"
-                    onClick={hadleClick}
+                    onClick={handleClick}
                 >
                     <i className="bi bi-gear"></i>
                 </button>
@@ -22,7 +22,7 @@ const UserCard = ({ user }) => {
                         )
                             .toString(36)
                             .substring(7)}.svg`}
-                        className="reunded-circle"
+                        className="rounded-circle"
                         width="150"
                     />
                     <div className="mt-3">
