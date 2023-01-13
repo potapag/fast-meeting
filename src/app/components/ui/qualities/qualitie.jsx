6b6392facf59d1e85a82c92c724443c6ebe6308e
potapag <potapag@gmail.com>
2023-01-13 14:42:07 +0300

    call server

--- src/app/components/ui/qualities/qualitie.jsx
+++ src/app/components/ui/qualities/qualitie.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useQualities } from '../../../hooks/useQualities';
 
-const Qualitie = ({ color, name, _id }) => {
+const Quality = ({ id }) => {
+    const { getQuality } = useQualities();
+    const { color, name } = getQuality(id);
     return <span className={'badge m-1 bg-' + color}>{name}</span>;
 };
-
-Qualitie.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired
+Quality.propTypes = {
+    id: PropTypes.string.isRequired
 };
 
-export default Qualitie;
+export default Quality;
--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Qualitie from './qualitie';
+import { useQualities } from '../../../hooks/useQualities';
 
 const QualitiesList = ({ qualities }) => {
+    const { isLoading } = useQualities();
+    if (isLoading) return 'Loading...';
+
     return (
         <>
             {qualities.map((qual) => (
